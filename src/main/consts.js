2634a290eac38fe1f903b368cc47f41e385fbba9
Douglas da silva lopes <douglas_on@hotmail.com>
2018-02-23 02:02:51 -0300

    'final'

--- src/main/consts.js
+++ src/main/consts.js
@@ -1,4 +1,4 @@
 export default {
-    API_URL: 'https://cash-money-backend.herokuapp.com/api',
-    OAPI_URL:'https://cash-money-backend.herokuapp.com/oapi',
+    API_URL: 'http://localhost:3003/api',
+    OAPI_URL:'http://localhost:3003/oapi',
 }
